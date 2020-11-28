34216fa5f0f98df05fead1f8fa61b393c315eae2
FC <springfeng@gmail.com>
2020-11-28 23:27:17 +0800

    add smash bros

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from cheaper import Handler
 
 if __name__ == "__main__":
-  event = {"game_list": ("Animal Crossing™: New Horizons","The Legend of Zelda: Breath of the Wild","Fitness Boxing","Fitness Boxing 2: Rhythm & Exercise","1-2-Switch", "Mario Kart™ 8 Deluxe", "Splatoon™ 2")}
+  event = {"game_list": ("Super Smash Bros.™ Ultimate","Animal Crossing™: New Horizons","The Legend of Zelda: Breath of the Wild","Fitness Boxing","Fitness Boxing 2: Rhythm & Exercise","1-2-Switch", "Mario Kart™ 8 Deluxe", "Splatoon™ 2")}
   Handler(event, None)
 
